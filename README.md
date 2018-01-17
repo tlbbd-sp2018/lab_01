5269efa938a9cdcb0ed1c3f032b44329e58c84ba
Nima Hejazi <nh@nimahejazi.org>
2018-01-17 01:03:35 -0800

    add binder badge; pkgs in install.r

---
+++ README.md
@@ -0,0 +1,1 @@
+# Labs 01 and 02 for TLBBD (Spring 2018) [![Binder](https://mybinder.org/badge.svg)](https://mybinder.org/v2/gh/tlbbd-spring2018/lab_01/master)
---
+++ install.r
@@ -0,0 +1,4 @@
+install.packages("tidyverse")
+install.packages("here")
+devtools::install_github("r-lib/usethis")
+devtools::install_github("ropenscilabs/skimr")
